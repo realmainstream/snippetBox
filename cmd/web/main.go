86ebdb8e1be4f5fbd725e91e04856dc85ed8cf5e
Zhandos <zhandoslp@gmail.com>
2025-04-08 21:53:36 +0500

    Refactored ServeMux routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -25,18 +25,11 @@ func main() {
 		errorLog: errorLog,
 		infoLog:  infoLog,
 	}
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 	infoLog.Printf("Starting server on a %v", *addr)
 	err := srv.ListenAndServe()
