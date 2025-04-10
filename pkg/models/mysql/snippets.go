6c92d0a36ec97e4dbde8c3587dbbfa1df4724ebb
Zhandos <zhandoslp@gmail.com>
2025-04-10 17:09:11 +0500

    Implement snippets Insert functionality

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -2,6 +2,7 @@ package mysql
 
 import (
 	"database/sql"
+	"strconv"
 
 	"github.com/toleubekov/snippetBox/pkg/models"
 )
@@ -11,7 +12,22 @@ type SnippetModel struct {
 }
 
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+
+	expiresInt, err := strconv.Atoi(expires)
+	if err != nil {
+		return 0, err
+	}
+
+	stmt := "INSERT INTO snippets (title, content, created, expires) VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))"
+	result, err := m.DB.Exec(stmt, title, content, expiresInt)
+	if err != nil {
+		return 0, err
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return int(id), nil
 }
 
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
